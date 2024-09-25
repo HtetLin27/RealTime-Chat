1d73e894c0c9ff9f4655042e21fb6fc9beb4dbcb
htetlynn <htetlynnko27@gmail.com>
2024-09-26 00:09:01 +0700

    chat component design

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -30,7 +30,78 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Nemo, sunt! Quas fuga autem libero provident 
+              unde quos vitae maxime perferendis illo, ad amet 
+              adipisci error repellat modi. Eius, ipsa in?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Nemo, sunt! Quas fuga autem libero provident 
+              unde quos vitae maxime perferendis illo, ad amet 
+              adipisci error repellat modi. Eius, ipsa in?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Nemo, sunt! Quas fuga autem libero provident 
+              unde quos vitae maxime perferendis illo, ad amet 
+              adipisci error repellat modi. Eius, ipsa in?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Nemo, sunt! Quas fuga autem libero provident 
+              unde quos vitae maxime perferendis illo, ad amet 
+              adipisci error repellat modi. Eius, ipsa in?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Nemo, sunt! Quas fuga autem libero provident 
+              unde quos vitae maxime perferendis illo, ad amet 
+              adipisci error repellat modi. Eius, ipsa in?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://images.pexels.com/photos/5706025/pexels-photo-5706025.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Nemo, sunt! Quas fuga autem libero provident 
+              unde quos vitae maxime perferendis illo, ad amet 
+              adipisci error repellat modi. Eius, ipsa in?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
