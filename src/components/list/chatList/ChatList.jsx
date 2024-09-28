a16d97d0f76a965a2d4b1adbed7aacf3e89e3c0f
htetlynn <htetlynnko27@gmail.com>
2024-09-28 23:50:36 +0700

    frontend design end

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 import './chatList.css'
+import AddUser from './addUser/AddUser'
 
 const ChatList = () => {
 
@@ -35,6 +36,7 @@ const ChatList = () => {
           <p>Hello</p>
         </div>
       </div>
+      { addMode && <AddUser/>}
     </div>
   )
 }
